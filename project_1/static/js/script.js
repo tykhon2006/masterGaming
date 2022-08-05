735b2cb463acc086eaa09b6431e3de429ca15638
tykhon2006 <tisha8690@gmail.com>
2022-08-05 18:03:24 +0200

    added media

--- project_1/static/js/script.js
+++ project_1/static/js/script.js
@@ -1,12 +1,27 @@
 function show_active_link(menu){
-        try{
-            let elem = document.getElementById(menu).querySelectorAll("a")
-            let url = document.location.href
-            for(link in elem){
-                if(elem[link].href == url){
-                    elem[link].classList.add("active")
-                }
+    try{
+        let elem = document.getElementById(menu).querySelectorAll("a")
+        let url = document.location.href
+        for(link in elem){
+            if(elem[link].href == url){
+                elem[link].classList.add("active")
             }
-        } catch{}
-    }
+        }
+    } catch{}
+}
 show_active_link("menu")
+
+function print_full_text(body){
+    try{
+        let elem = document.getElementById(body).querySelectorAll("a")
+        let url = document.location.href
+//        console.log(url)
+
+        for(link in elem){
+            if(elem[link].href == url){
+                document.createElement(element)
+            }
+        }
+    } catch{}
+}
+print_full_text("menu")
